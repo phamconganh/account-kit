fbd10d482a2295cd261dfd3f3c0ec77602549e3a
phamconganh <conganhphamnd@gmail.com>
2018-04-06 11:57:40 +0700

    config

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const Mustache  = require('mustache');
 const app = express();
-const db = require('./models/db.js');
+const db = require('./models/db');
+const connect = require('./config/database')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -11,17 +12,13 @@ const sign_in = require('./routes/signIn.js');
 
 const forgot_password = require('./routes/forgotPassword.js')
 
-const dbName = 'accountKit';
-
-const urlDB = 'mongodb://localhost:27017/' + dbName;
-
 const port = 3000;
 
 app.use('/sign_in',sign_in);
 
 app.use('/forgot_password',forgot_password);
 
-db.connect(urlDB, function(err) {
+db.connect(connect.database, function(err) {
   if (err) {
     console.log('Unable to connect to Mongo.');
     db.close();
